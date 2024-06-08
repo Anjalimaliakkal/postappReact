803cd9e142c74a1a8a199b72a7cf86c41d47a9eb
Anjalimaliakkal <anjalimsanjali2@gmail.com>
2024-06-08 10:37:18 +0530

    project created

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -609,40 +609,35 @@ const ViewAll = () => {
     return (
 
         <div>
-            <NavBar/>
+            <NavBar />
             <h1><center>VIEWALL POSTS</center></h1>
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <div className="row">
-                            {
-                                
-                                data.map(
-                                    (value, index) => {
-                                        return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                            <table class="table">
-                                                <thead>
-                                                    <tr>
-                                                        <th scope="col">USER ID</th>
-                                                        <th scope="col">ID</th>
-                                                        <th scope="col">TITLE</th>
-                                                        <th scope="col">BODY</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <th scope="row">{value.userId}</th>
-                                                        <td>{value.id}</td>
-                                                        <td>{value.title}</td>
-                                                        <td>{value.body}</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    }
-                                )
-                            }
-                        </div>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">USER ID</th>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">TITLE</th>
+                                    <th scope="col">BODY</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    data.map(
+                                        (value, index) => {
+                                            return <tr>
+                                                <th scope="row">{value.userId}</th>
+                                                <td>{value.id}</td>
+                                                <td>{value.title}</td>
+                                                <td>{value.body}</td>
+                                            </tr>
+                                        }
+                                    )
+                                }
+                                        </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
